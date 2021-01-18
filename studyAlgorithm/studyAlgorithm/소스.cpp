c541659bc9d53f6bff6847c707c6a93035dee086
KDH20160140 <bzidol@naver.com>
2021-01-18 10:18:06 +0900

    21/01/18 백준 1913

--- studyAlgorithm/studyAlgorithm/소스.cpp
+++ studyAlgorithm/studyAlgorithm/소스.cpp
@@ -1,38 +1,109 @@
-#include <iostream>
-#include <vector>
-#include <string.h>
-#include <algorithm>
-
+#include<iostream>
 using namespace std;
-//20291
+
+//1913
 int main() {
 	int N = 0;
-	string input;
+	int find = 0;
+	int x = 0, y = 0;
 	cin >> N;
-	vector<string> name;
-	int index = 1;
+	cin >> find;
 
+	int** arr = new int*[N];
 	for (int i = 0; i < N; i++) {
-		cin >> input;
-		string temp = input.erase(0, input.find(".") + 1);
-		name.push_back(temp);
+		arr[i] = new int[N];
+		memset(arr[i], 0, sizeof(int) * N);
 	}
 
-	sort(name.begin(), name.end());
+	int arrx = 0, arry = 0;
 
-	for (int i = 0; i < N; i++) {
-		if (i != 0) {
-			if (name[i] == name[i - 1]) {
-				index++;
+	//입력값 제곱수
+	int N2 = N * N;
+	arr[arrx++][arry] = N2--;
+	
+	/*
+	1. 아래
+	2. 오른쪽
+	3. 위
+	4. 왼쪽
+	순서로 돌면서 값이 0이 아닐때 까지 반복
+	
+	*/
+
+	while (N2 > 1) {
+		//1.
+		while (arrx < N) {
+			if (arr[arrx][arry] == 0) {
+				arr[arrx][arry] = N2--;
+				arrx++;
 			}
-			else if(name[i] != name[i-1]){
-				cout << name[i - 1] << " " << index << "\n";
-				index = 1;
+			else {
+				break;
 			}
-			if (i + 1 == N)
-				cout << name[i] << " " << index << "\n";
 		}
+		arry++;
+		arrx--;
+
+		//2.
+		while (arry < N) {
+			if (arr[arrx][arry] == 0) {
+				arr[arrx][arry] = N2--;
+				arry++;
+			}
+			else {
+				break;
+			}
+		}
+		arrx--;
+		arry--;
+
+		//3.
+		while (arrx >= 0) {
+			if (arr[arrx][arry] == 0) {
+				arr[arrx][arry] = N2--;
+				arrx--;
+			}
+			else {
+				break;
+			}
+		}
+		arrx++;
+		arry--;
+
+		//4.
+		while (arry >= 0) {
+			if (arr[arrx][arry] == 0) {
+				arr[arrx][arry] = N2--;
+				arry--;
+			}
+			else {
+				break;
+			}
+		}
+		arrx++;
+		arry++;
+	}
+	arr[arrx][arry] = 1;
+
+	//표 출력
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			//그리다가 현재 찍을 값이 find 면 i,j 값을 x,y 에 저장.
+			cout << arr[i][j] << " ";
+			if (arr[i][j] == find) {
+				x = i+1;
+				y = j+1;
+			}
+		}
+		cout << "\n";
 	}
 
+	//값 출력
+	cout << x << " " << y << "\n";
+
+	//해제
+	for (int i = 0; i < N; i++) {
+		delete[] arr[i];
+	}
 	return 0;
 }
